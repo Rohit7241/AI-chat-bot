a7c4978ed78f0faf46a2c6cca97b22cbf9546dc7
RohitBadakali <168419797+Rohit7241@users.noreply.github.com>
2025-04-11 01:21:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   createUserContent,
   createPartFromUri,
 } from "@google/genai";
-const ai = new GoogleGenAI({ apiKey: AIzaSyAVvgKim9IZYOCe7nfU1bjkBZH9AHxQ1ac  });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyAVvgKim9IZYOCe7nfU1bjkBZH9AHxQ1ac"  });
 function App() {
   const [messages,setmessages]=useState([]);
   function addmessage(message){
