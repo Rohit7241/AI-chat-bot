cb60ae8f7e2033a6d277c68b2c46c2b21d9f52ec
Rohit7241 <rohitbadakali7241@gmail.com>
2025-04-13 11:43:34 +0530

    response streaming

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,35 @@ import {
   createUserContent,
   createPartFromUri,
 } from "@google/genai";
-const ai = new GoogleGenAI({ apiKey: "AIzaSyAVvgKim9IZYOCe7nfU1bjkBZH9AHxQ1ac"  });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyDWRJAbDv_LWieYHaQfoe3iWlickhb-SCg"  });
 function App() {
   const [loading,setload]=useState(false);
   const [messages,setmessages]=useState([]);
   function addmessage(message){
     setmessages((prev)=>[...prev,message])
   }
+  function updatemessage(chunks){
+    setmessages((prev)=>
+    prev.map((message,index)=>
+    index===prev.length-1?{...message,content:`${message.content}${chunks.candidates[0].content.parts[0].text}`}:message))
+  }
   async function onsend(content ){
     addmessage({role:"user",content});
     setload(true);
     try {
-      const response = await ai.models.generateContent({
+      const response = await ai.models.generateContentStream({
         model: "gemini-1.5-flash-8b",
         contents: content,
       })
+      let isfirstchunk=false;
+      for await(const chunks of response){
+         if(!isfirstchunk){
+          isfirstchunk=true;
+          addmessage({role:"assistant",content:""}); 
+        }
+         updatemessage(chunks);
+      }
       setload(false);
-      addmessage({role:"assistant",content:response.candidates[0].content.parts[0].text}); 
-     
     } catch (error) {
       setload(false);
       addmessage({role:"system",content:"Sorry, the request coudnt be processed "});    
@@ -38,7 +49,7 @@ function App() {
       <header className={styles.header}>
       <img className={styles.logo} src="/chatbot.png" alt="" />
       <h2 className={styles.title}>AI CHAT BOT</h2>
-      {loading &&<p className={styles.message} style={{display:"flex"}}>ChatBot is typing<div className={styles.loader}></div></p>}
+      {loading &&<div className={styles.message} style={{display:"flex"}}>ChatBot is typing<div className={styles.loader}></div></div>}
       </header>
       <div className={styles.chatcontainer}>
         <Chat messages={messages} loading={loading}/>
